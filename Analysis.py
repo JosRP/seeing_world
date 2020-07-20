036478c6771f5a200a13c6a78ff88464a8d870f3
JosRP <josericardo221193@gmail.com>
2020-07-20 17:44:55 +0100

    First disappointment

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import tree
 
-pd.options.display.html.table_schema = True
+pd.options.display.html.table_schema = False
 pd.options.display.max_rows = 100
 
 data_AMD = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, SA/Pessoal/Python/Stocks/Data/AMD.csv", parse_dates =["Date"])
@@ -11,61 +14,139 @@ data_SPY = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, S
 df_AMD = pd.DataFrame(data_AMD)
 df_SPY = pd.DataFrame(data_SPY)
 
-del df_AMD["Adj Close"]
-del df_SPY["Adj Close"]
+del df_AMD["Close"]
+del df_SPY["Close"]
 
 df_Total = pd.merge(df_AMD, df_SPY, on="Date", how="inner")
 
 df_Total.rename(columns={'Open_x':'Open',
                           'High_x':'High',
                           'Low_x':'Low',
-                          'Close_x':'Close',
+                          'Adj Close_x':'Close',
                           'Volume_x':'Volume',
                           'Open_y':'Open_SPY',
                           'High_y':'High_SPY',
                           'Low_y':'Low_SPY',
-                          'Close_y':'Close_SPY',
+                          'Adj Close_y':'Close_SPY',
                           'Volume_y':'Volume_SPY'},
                           inplace=True)
 
 # Rolling AVG AMD
-df_Total["MA(5)"] = df_Total["Close"].rolling(5).mean()
-df_Total["MA(10)"] = df_Total["Close"].rolling(10).mean()
-df_Total["MA(50)"] = df_Total["Close"].rolling(50).mean()
-df_Total["MA(100)"] = df_Total["Close"].rolling(100).mean()
-df_Total["MA(150)"] = df_Total["Close"].rolling(150).mean()
-df_Total["MA(200)"] = df_Total["Close"].rolling(200).mean()
+df_Total["MA(5)"] = round(df_Total["Close"].rolling(5).mean(),2)
+df_Total["MA(10)"] = round(df_Total["Close"].rolling(10).mean(),2)
+df_Total["MA(50)"] = round(df_Total["Close"].rolling(50).mean(),2)
+df_Total["MA(100)"] = round(df_Total["Close"].rolling(100).mean(),2)
+df_Total["MA(150)"] = round(df_Total["Close"].rolling(150).mean(),2)
+df_Total["MA(200)"] = round(df_Total["Close"].rolling(200).mean(),2)
 
 # Rolling AVG SPY
-df_Total["MA(5) SPY"] = df_Total["Close_SPY"].rolling(5).mean()
-df_Total["MA(10) SPY"] = df_Total["Close_SPY"].rolling(10).mean()
-df_Total["MA(50) SPY"] = df_Total["Close_SPY"].rolling(50).mean()
-df_Total["MA(100) SPY"] = df_Total["Close_SPY"].rolling(100).mean()
-df_Total["MA(150) SPY"] = df_Total["Close_SPY"].rolling(150).mean()
-df_Total["MA(200) SPY"] = df_Total["Close_SPY"].rolling(200).mean()
+df_Total["MA(5)_SPY"] = round(df_Total["Close_SPY"].rolling(5).mean(),2)
+df_Total["MA(10)_SPY"] = round(df_Total["Close_SPY"].rolling(10).mean(),2)
+df_Total["MA(50)_SPY"] = round(df_Total["Close_SPY"].rolling(50).mean(),2)
+df_Total["MA(100)_SPY"] = round(df_Total["Close_SPY"].rolling(100).mean(),2)
+df_Total["MA(150)_SPY"] = round(df_Total["Close_SPY"].rolling(150).mean(),2)
+df_Total["MA(200)_SPY"] = round(df_Total["Close_SPY"].rolling(200).mean(),2)
+
+# Round Close_x
+df_Total["Close"] = round(df_Total["Close"],2)
+df_Total["Close_SPY"] = round(df_Total["Close_SPY"],2)
 
 # How much Growth % next 5 days
-df_Total["Max_High_Next(5)%"] = (df_Total["High"].rolling(5).max().shift(-5)/df_Total["Close"])-1
+df_Total["Max(5)"] = round((df_Total["High"].rolling(5).max().shift(-5)/df_Total["Close"])-1,2)
 
 # Did Grown more than 5% next 5 days?
-df_Total["Max_High_Next(5)?"] = np.where(df_Total["Max_High_Next(5)%"]>=0.05, "Y", "N")
+df_Total["High(5)"] = np.where(df_Total["Max(5)"]>=0.05, "1", "0")
 
 # Relative Volume last 10 days
-df_Total["Rel. Vol (10)"] = df_Total["Volume"]/(df_Total["Volume"].rolling(10).mean())-1
-df_Total["Rel. Vol (10) SPY"] =  df_Total["Volume_SPY"]/(df_Total["Volume_SPY"].rolling(10).mean())-1
+df_Total["Rel. Vol(10)"] = round(df_Total["Volume"]/(df_Total["Volume"].rolling(10).mean())-1,2)
+df_Total["Rel. Vol(10)_SPY"] =  round(df_Total["Volume_SPY"]/(df_Total["Volume_SPY"].rolling(10).mean())-1,2)
 
 # Date as Index
 df_Total.set_index('Date', inplace=True)
 
-# RSI
-RSI = df_Total.loc[:,["Close","Close_SPY"]]
-RSI["Close_Change"] = RSI["Close"]-RSI["Close"].shift(1)
-RSI["Close_SPY_Change"] = RSI["Close_SPY"]-RSI["Close_SPY"].shift(1)
+## RSI
+window_length = 14
+# Change in price
+delta = df_Total['Close'].diff()
+delta_SPY = df_Total['Close_SPY'].diff()
+# Gain & Loss Series
+up, down = delta.copy(), delta.copy()
+up[up < 0] = 0
+down[down > 0] = 0
+up_SPY, down_SPY = delta_SPY.copy(), delta_SPY.copy()
+up_SPY[up_SPY < 0] = 0
+down_SPY[down_SPY > 0] = 0
+# Calculate SMA
+roll_up = up.rolling(window_length).mean()
+roll_down = down.abs().rolling(window_length).mean()
+roll_up_SPY = up_SPY.rolling(window_length).mean()
+roll_down_SPY = down_SPY.abs().rolling(window_length).mean()
+# Calculate the RSI based on SMA
+RS = roll_up / roll_down
+RSI = 100.0 - (100.0 / (1.0 + RS))
+RS_SPY = roll_up_SPY / roll_down_SPY
+RSI_SPY = 100.0 - (100.0 / (1.0 + RS_SPY))
+# Merge to Main Dataframe
+df_Total['RSI'] = RSI
+df_Total['RSI_SPY'] = RSI_SPY
 
-RSI["Gain"] = np.where(RSI["Close_Change"] > 0, RSI["Close_Change"], np.nan)
-RSI["Gain_SPY"] = np.where(RSI["Close_SPY_Change"] > 0, RSI["Close_SPY_Change"], np.nan)
+# Create relative MA Columns
+df_Total['5>10'] = np.where(df_Total["MA(5)"]>df_Total["MA(10)"], "1", "0")
+df_Total['10>50'] = np.where(df_Total["MA(10)"]>df_Total["MA(50)"], "1", "0")
+df_Total['50>100'] = np.where(df_Total["MA(50)"]>df_Total["MA(100)"], "1", "0")
+df_Total['100>150'] = np.where(df_Total["MA(100)"]>df_Total["MA(150)"], "1", "0")
+df_Total['150>200'] = np.where(df_Total["MA(150)"]>df_Total["MA(200)"], "1", "0")
 
-RSI["Loss"] = np.where(RSI["Close_Change"] < 0, RSI["Close_Change"], np.nan)
-RSI["Loss_SPY"] = np.where(RSI["Close_SPY_Change"] < 0, RSI["Close_SPY_Change"], np.nan)
+df_Total['5>10_SPY'] = np.where(df_Total["MA(5)_SPY"]>df_Total["MA(10)_SPY"], "1", "0")
+df_Total['10>50_SPY'] = np.where(df_Total["MA(10)_SPY"]>df_Total["MA(50)_SPY"], "1", "0")
+df_Total['50>100_SPY'] = np.where(df_Total["MA(50)_SPY"]>df_Total["MA(100)_SPY"], "1", "0")
+df_Total['100>150_SPY'] = np.where(df_Total["MA(100)_SPY"]>df_Total["MA(150)_SPY"], "1", "0")
+df_Total['150>200_SPY'] = np.where(df_Total["MA(150)_SPY"]>df_Total["MA(200)_SPY"], "1", "0")
 
-RSI.head(35)
+# Clean columns
+data=df_Total.loc[:,['High(5)', 'Max(5)', 'Rel. Vol(10)',
+       'Rel. Vol(10)_SPY', 'RSI', 'RSI_SPY', '5>10', '10>50', '50>100',
+       '100>150', '150>200', '5>10_SPY', '10>50_SPY', '50>100_SPY',
+       '100>150_SPY', '150>200_SPY']]
+
+# Remove NAN convert objects to int
+data = data.dropna()
+data[['High(5)',
+    '5>10',
+    '10>50',
+    '50>100',
+    '100>150',
+    '150>200',
+    '5>10_SPY',
+    '10>50_SPY',
+    '50>100_SPY',
+    '100>150_SPY',
+    '150>200_SPY']] = data[['High(5)',
+    '5>10',
+    '10>50',
+    '50>100',
+    '100>150',
+    '150>200',
+    '5>10_SPY',
+    '10>50_SPY',
+    '50>100_SPY',
+    '100>150_SPY',
+    '150>200_SPY']].apply(pd.to_numeric)
+
+# EDA
+sns.scatterplot(x='Rel. Vol(10)',y='Max(5)',data=data)
+plt.close()
+sns.scatterplot(x='RSI',y='Max(5)',data=data)
+plt.close()
+sns.boxplot(x='5>10',y='Max(5)',data=data)
+plt.close()
+
+##Classification Tree
+# Convert Target to string
+data['Max(5)'] = data['Max(5)'].apply(str)
+data.dtypes
+
+X = data.drop(['High(5)','Max(5)'],axis=1).values
+y = data['Max(5)'].values
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
