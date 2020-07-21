f2a91131aafc375749a91e9013aedcb9d2abd6f3
JosRP <josericardo221193@gmail.com>
2020-07-21 23:33:42 +0100

    Minor Changes

--- Analysis.py
+++ Analysis.py
@@ -6,15 +6,19 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+# Need update bases
+# Need find way to remove -2% if befoire max 5%
 
 pd.options.display.html.table_schema = False
 pd.options.display.max_rows = 100
 
-#data_AMD = pd.read_csv("C:/Users/joser/Google Drive/Projects/Python/Stocks/Data/AMD.csv", parse_dates =["Date"])
-#data_SPY = pd.read_csv("C:/Users/joser/Google Drive/Projects/Python/Stocks/Data/SPY.csv", parse_dates =["Date"])
+data_AMD = pd.read_csv("C:/Users/joser/Google Drive/Projects/Python/Stocks/Data/AMD.csv", parse_dates =["Date"])
+data_SPY = pd.read_csv("C:/Users/joser/Google Drive/Projects/Python/Stocks/Data/SPY.csv", parse_dates =["Date"])
 
-data_AMD = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, SA/Pessoal/Python/Stocks/Data/AMD.csv", parse_dates =["Date"])
-data_SPY = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, SA/Pessoal/Python/Stocks/Data/SPY.csv", parse_dates =["Date"])
+#data_AMD = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, SA/Pessoal/Python/Stocks/Data/AMD.csv", parse_dates =["Date"])
+#data_SPY = pd.read_csv("C:/Users/jrpgo/OneDrive - Rigor Consultoria e Gestão, SA/Pessoal/Python/Stocks/Data/SPY.csv", parse_dates =["Date"])
 
 df_AMD = pd.DataFrame(data_AMD)
 df_SPY = pd.DataFrame(data_SPY)
@@ -34,11 +38,10 @@ df_Total.rename(columns={'Open_x':'Open',
                           'Low_y':'Low_SPY',
                           'Adj Close_y':'Close_SPY',
                           'Volume_y':'Volume_SPY'},
+                          inplace=True)
 
-#                          inplace=True)
+# Add day variable
 days = 5
-max = 'Max(' + str(days) +  ')'
-high = 'High(' + str(days) +  ')'
 
 # Rolling AVG AMD
 df_Total["MA(5)"] = round(df_Total["Close"].rolling(5).mean(),2)
@@ -60,11 +63,12 @@ df_Total["MA(200)_SPY"] = round(df_Total["Close_SPY"].rolling(200).mean(),2)
 df_Total["Close"] = round(df_Total["Close"],2)
 df_Total["Close_SPY"] = round(df_Total["Close_SPY"],2)
 
-# How much Growth % next 5 days
-df_Total["Max(5)"] = round((df_Total["High"].rolling(5).max().shift(-5)/df_Total["Close"])-1,2)
+# How much Max and Min Growth % next 5 days
+df_Total["Max(x)"] = round((df_Total["High"].rolling(days).max().shift(-5)/df_Total["Close"])-1,2)
+#df_Total["Min(x)"] = round((df_Total["High"].rolling(days).min().shift(-5)/df_Total["Close"])-1,2)
 
 # Did Grown more than 5% next 5 days?
-df_Total["High(5)"] = np.where(df_Total["Max(5)"]>=0.05, "1", "0")
+df_Total["Target(x)"] = np.where(df_Total["Max(x)"]>=0.05, "1", "0")
 
 # Relative Volume last 10 days
 df_Total["Rel. Vol(10)"] = round(df_Total["Volume"]/(df_Total["Volume"].rolling(10).mean())-1,2)
@@ -113,14 +117,14 @@ df_Total['100>150_SPY'] = np.where(df_Total["MA(100)_SPY"]>df_Total["MA(150)_SPY
 df_Total['150>200_SPY'] = np.where(df_Total["MA(150)_SPY"]>df_Total["MA(200)_SPY"], "1", "0")
 
 # Clean columns
-data=df_Total.loc[:,['High(5)', 'Max(5)', 'Rel. Vol(10)',
+data=df_Total.loc[:,['Target(x)', 'Max(x)', 'Rel. Vol(10)',
        'Rel. Vol(10)_SPY', 'RSI', 'RSI_SPY', '5>10', '10>50', '50>100',
        '100>150', '150>200', '5>10_SPY', '10>50_SPY', '50>100_SPY',
        '100>150_SPY', '150>200_SPY']]
 
 # Remove NAN convert objects to int
 data = data.dropna()
-data[['High(5)',
+data[['Target(x)',
     '5>10',
     '10>50',
     '50>100',
@@ -130,7 +134,7 @@ data[['High(5)',
     '10>50_SPY',
     '50>100_SPY',
     '100>150_SPY',
-    '150>200_SPY']] = data[['High(5)',
+    '150>200_SPY']] = data[['Target(x)',
     '5>10',
     '10>50',
     '50>100',
@@ -143,19 +147,19 @@ data[['High(5)',
     '150>200_SPY']].apply(pd.to_numeric)
 
 # EDA
-sns.scatterplot(x='Rel. Vol(10)',y='Max(5)',data=data)
+sns.scatterplot(x='Rel. Vol(10)',y='Max(x)',data=data)
 plt.close()
-sns.scatterplot(x='RSI',y='Max(5)',data=data)
+sns.scatterplot(x='RSI',y='Max(x)',data=data)
 plt.close()
-sns.boxplot(x='5>10',y='Max(5)',data=data)
+sns.boxplot(x='5>10',y='Max(x)',data=data)
 plt.close()
 
 ##Classification Tree
 # Convert Target to string
-data['High(5)'] = data['High(5)'].apply(str)
+data['Target(x)'] = data['Target(x)'].apply(str)
 # Create train and test data. 42 for reproducibility
-X = data.drop(['High(5)','Max(5)'],axis=1).values
-y = data['High(5)'].values
+X = data.drop(['Target(x)','Max(x)'],axis=1).values
+y = data['Target(x)'].values
 data_features = ['Rel. Vol(10)', 'Rel. Vol(10)_SPY', 'RSI', 'RSI_SPY', '5>10', '10>50',
        '50>100', '100>150', '150>200', '5>10_SPY', '10>50_SPY', '50>100_SPY',
        '100>150_SPY', '150>200_SPY']
@@ -169,3 +173,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 clf = clf.fit(X_train, y_train)
 y_predict = clf.predict(X_test)
 confusion_matrix(y_test, y_predict)
+
+
+print(classification_report(y_test, y_predict))
