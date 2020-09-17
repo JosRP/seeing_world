d0c6c8614a10fcb8fbfba6e0c86ce0d6816214d6
JosRP <josericardo221193@gmail.com>
2020-09-17 18:04:21 +0100

    Changes

--- Analysis.py
+++ Analysis.py
@@ -11,17 +11,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.utils import resample
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import dask_ml.model_selection as dcv
 import yfinance as yf
 import pickle
 import itertools
 
-
-# Define Functions
+## Define Functions
+# Create target (check if target value occures before acceptable drop)
 def chrono_var(days,df,close_column,low_column,high_column,minimum,maximum):
     df = df.reset_index()
     for i in range((len(df.index)-days)):
@@ -41,6 +43,16 @@ def chrono_var(days,df,close_column,low_column,high_column,minimum,maximum):
             else:
                 df.loc[i,'Target(x)'] = 'Not'
                 continue
+    return df
+# Shift columns X times
+def multi_shift(df,columns,shift):
+    for i in columns:
+        if shift>1:
+            y=1
+        if shift<1:
+            y=-1
+        for t in range(1,(shift+y)):
+            df.loc[:,str(i)+"_("+str(t) +")"]=df[str(i)].shift(t)
     return df
 
 first_day = "2010-01-01"
@@ -89,7 +101,7 @@ df_Total["MA(150)_SPY"] = round(df_Total["Close_SPY"].rolling(150).mean(),2)
 df_Total["MA(200)_SPY"] = round(df_Total["Close_SPY"].rolling(200).mean(),2)
 
 # Create Label/Target variable
-df_Total = chrono_var(5,df_Total,"Close","Low","High",-0.02,0.05)
+df_Total = chrono_var(5,df_Total,"Close","Low","High",-0.02,0.10)
 
 # Relative Volume last 10 days
 df_Total["Rel. Vol(10)"] = round(df_Total["Volume"]/(df_Total["Volume"].rolling(10).mean())-1,2)
@@ -122,17 +134,17 @@ df_Total['RSI'] = RSI
 df_Total['RSI_SPY'] = RSI_SPY
 
 # Create relative MA Columns
-df_Total['5>10'] = np.where(df_Total["MA(5)"]>df_Total["MA(10)"], "1", "0")
-df_Total['10>50'] = np.where(df_Total["MA(10)"]>df_Total["MA(50)"], "1", "0")
-df_Total['50>100'] = np.where(df_Total["MA(50)"]>df_Total["MA(100)"], "1", "0")
-df_Total['100>150'] = np.where(df_Total["MA(100)"]>df_Total["MA(150)"], "1", "0")
-df_Total['150>200'] = np.where(df_Total["MA(150)"]>df_Total["MA(200)"], "1", "0")
+df_Total['5>10'] = np.where(df_Total["MA(5)"]>df_Total["MA(10)"], 1, 0)
+df_Total['10>50'] = np.where(df_Total["MA(10)"]>df_Total["MA(50)"], 1, 0)
+df_Total['50>100'] = np.where(df_Total["MA(50)"]>df_Total["MA(100)"], 1, 0)
+df_Total['100>150'] = np.where(df_Total["MA(100)"]>df_Total["MA(150)"], 1, 0)
+df_Total['150>200'] = np.where(df_Total["MA(150)"]>df_Total["MA(200)"], 1, 0)
 
-df_Total['5>10_SPY'] = np.where(df_Total["MA(5)_SPY"]>df_Total["MA(10)_SPY"], "1", "0")
-df_Total['10>50_SPY'] = np.where(df_Total["MA(10)_SPY"]>df_Total["MA(50)_SPY"], "1", "0")
-df_Total['50>100_SPY'] = np.where(df_Total["MA(50)_SPY"]>df_Total["MA(100)_SPY"], "1", "0")
-df_Total['100>150_SPY'] = np.where(df_Total["MA(100)_SPY"]>df_Total["MA(150)_SPY"], "1", "0")
-df_Total['150>200_SPY'] = np.where(df_Total["MA(150)_SPY"]>df_Total["MA(200)_SPY"], "1", "0")
+df_Total['5>10_SPY'] = np.where(df_Total["MA(5)_SPY"]>df_Total["MA(10)_SPY"], 1, 0)
+df_Total['10>50_SPY'] = np.where(df_Total["MA(10)_SPY"]>df_Total["MA(50)_SPY"], 1, 0)
+df_Total['50>100_SPY'] = np.where(df_Total["MA(50)_SPY"]>df_Total["MA(100)_SPY"], 1, 0)
+df_Total['100>150_SPY'] = np.where(df_Total["MA(100)_SPY"]>df_Total["MA(150)_SPY"], 1, 0)
+df_Total['150>200_SPY'] = np.where(df_Total["MA(150)_SPY"]>df_Total["MA(200)_SPY"], 1, 0)
 
 # Low, High, Open Relative Position to Close
 df_Total['Low Pos'] = round(df_Total['Low']/df_Total['Close']-1,2)
@@ -143,8 +155,7 @@ df_Total['Low Pos_SPY'] = round(df_Total['Low_SPY']/df_Total['Close_SPY']-1,2)
 df_Total['High Pos_SPY'] = round(df_Total['High_SPY']/df_Total['Close_SPY']-1,2)
 df_Total['Open Pos_SPY'] = round(df_Total['Open_SPY']/df_Total['Close_SPY']-1,2)
 
-# Remove NaN & Clean columns
-df_Total = df_Total.dropna()
+# Apply time delay & Remove NaN & Clean columns
 data=df_Total.loc[:,['MA(5)', 'MA(10)',
        'MA(50)', 'MA(100)', 'MA(150)', 'MA(200)', 'MA(5)_SPY', 'MA(10)_SPY',
        'MA(50)_SPY', 'MA(100)_SPY', 'MA(150)_SPY', 'MA(200)_SPY', 'Target(x)',
@@ -152,10 +163,8 @@ data=df_Total.loc[:,['MA(5)', 'MA(10)',
        '50>100', '100>150', '150>200', '5>10_SPY', '10>50_SPY', '50>100_SPY',
        '100>150_SPY', '150>200_SPY', 'Low Pos', 'High Pos', 'Open Pos',
        'Low Pos_SPY', 'High Pos_SPY', 'Open Pos_SPY']]
-
-#  Convert objects to int
-cols=['5>10', '10>50', '50>100', '100>150', '150>200', '5>10_SPY', '10>50_SPY', '50>100_SPY', '100>150_SPY', '150>200_SPY']
-data[cols] = data[cols].apply(pd.to_numeric)
+data = multi_shift(data,data.columns[data.columns!="Target(x)"],5)
+data = data.dropna()
 
 ## Split Train Test ORDERED
 train, test= np.split(data, [int(.70 *len(data))])
@@ -174,22 +183,23 @@ train_balanced = pd.concat([train_majority, minority_upsample])
 X_train =  train_balanced.drop(['Target(x)'],axis=1)
 y_train = train_balanced['Target(x)']
 
-# Force Split
-split_index = [-1 if x in X_train.index else 0 for x in data.index]
+# Force Split in GridSearch
+split_index = [-1]*int(len(X_train)*0.7)+[0]*(len(X_train)-int(len(X_train)*0.7))
 pds = PredefinedSplit(test_fold = split_index)
 
 ########################## Logistic Regression
 # Create Steps & Pipeline
 steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
 ('logistic', LogisticRegression())]
 pipeline = Pipeline(steps)
 # Hyperperameters
+n_components = [20,40,80,120,190]
 penalty = ['none']
-C = np.logspace(-4, 4, 20)
 solver = ['lbfgs']
-parameters = {'logistic__penalty':penalty,'logistic__C':C,'logistic__solver':solver,'logistic__max_iter':[5000]}
+parameters = {"PCA__n_components":n_components,'logistic__penalty':penalty,'logistic__solver':solver,'logistic__max_iter':[1000000000]}
 # Fit and Evaluate
-GS_Log = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',error_score=0.0,scoring='roc_auc')
+GS_Log = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',error_score=0.0,scoring='roc_auc',cv=pds)
 GS_Log.fit(X_train, y_train)
 GS_Log.score(X_test,y_test)
 y_pred = GS_Log.predict(X_test)
@@ -199,16 +209,39 @@ y_pred_prob = GS_Log.predict_proba(X_test)[:,0]
 fpr_log, tpr_log, thresholds_log = roc_curve(y_test, y_pred_prob, pos_label="Buy")
 LogisticRegression_AUC = auc(fpr_log, tpr_log)
 
+########################## Bagging
+steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
+('Bag', BaggingClassifier())]
+pipeline = Pipeline(steps)
+# Hyperperameters
+n_components = [20,40,80,120,190]
+base_estimator = [DecisionTreeClassifier()]
+n_estimators=[5,10,15,20,50,100]
+parameters = {"PCA__n_components":n_components,"Bag__base_estimator":base_estimator,"Bag__n_estimators":n_estimators}
+# Fit and Evaluate
+GS_Bag = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',error_score=0.0,scoring='roc_auc',cv=pds)
+GS_Bag.fit(X_train, y_train)
+GS_Bag.score(X_test,y_test)
+y_pred = GS_Bag.predict(X_test)
+Logistic_report = classification_report(y_test, y_pred)
+# Create Log ROC Curve Variables
+y_pred_prob = GS_Bag.predict_proba(X_test)[:,0]
+fpr_log, tpr_log, thresholds_log = roc_curve(y_test, y_pred_prob, pos_label="Buy")
+Bagging_AUC = auc(fpr_log, tpr_log)
+
 ########################## Random Forest
 steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
 ('forest', RandomForestClassifier())]
 pipeline = Pipeline(steps)
 #Hyperperameters
+n_components = [20,40,80,120,190]
 criterion = ['gini', 'entropy']
 max_depth = np.arange(5, 50, 2)
-parameters = {'forest__criterion':criterion,'forest__max_depth':max_depth, 'forest__random_state':[1]}
+parameters = {"PCA__n_components":n_components,'forest__criterion':criterion,'forest__max_depth':max_depth, 'forest__random_state':[1]}
 #Fit and Evaluate
-GS_Forest = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',scoring='roc_auc')
+GS_Forest = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',cv=pds)
 GS_Forest.fit(X_train, y_train)
 y_pred = GS_Forest.predict(X_test)
 RandomForest_report =  classification_report(y_test, y_pred)
@@ -219,15 +252,17 @@ RandomForest_AUC = auc(fpr_forest, tpr_forest)
 
 ########################## KNN
 steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
 ('Knn', KNeighborsClassifier())]
 pipeline = Pipeline(steps)
 # Hyperperameters
+n_components = [20,40,80,120,190]
 weights = ['uniform', 'distance']
 algorithm = ['ball_tree','kd_tree','brute']
 n_neighbors = np.arange(5, 50, 2)
-parameters = {'Knn__algorithm':algorithm,'Knn__weights':weights,'Knn__n_neighbors':n_neighbors}
+parameters = {"PCA__n_components":n_components,'Knn__algorithm':algorithm,'Knn__weights':weights,'Knn__n_neighbors':n_neighbors}
 # Fit and Evaluate
-GS_Knn = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',scoring='roc_auc')
+GS_Knn = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',cv=pds)
 GS_Knn.fit(X_train, y_train)
 GS_Knn.score(X_test,y_test)
 y_pred = GS_Knn.predict(X_test)
@@ -240,16 +275,18 @@ Knn_AUC = auc(fpr_Knn, tpr_Knn)
 
 ########################## Neural Network
 steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
 ('MLP', MLPClassifier())]
 pipeline = Pipeline(steps)
 # Hyperperameters
-hidden_layer_sizes = [(50,50,50), (50,100,50)]
-solver = ['lbfgs']
-alpha = [0.0001,0.001,0.01,0.1]
+n_components = [20,40,80,120,190]
+hidden_layer_sizes = [(190,50,50), (190,100,50),(190,50,2), (190,100,2),(190,50,1), (190,100,1)]
+solver = ['lbfgs','sgd']
+alpha = [0.00001,0.0001,0.001,0.01,0.1]
 learning_rate = ['constant','adaptive']
-parameters = {'MLP__hidden_layer_sizes':hidden_layer_sizes,'MLP__solver':solver,'MLP__alpha':alpha,'MLP__learning_rate':learning_rate,'MLP__random_state':[42],'MLP__max_iter':[10000]}
+parameters = {"PCA__n_components":n_components,'MLP__hidden_layer_sizes':hidden_layer_sizes,'MLP__solver':solver,'MLP__alpha':alpha,'MLP__learning_rate':learning_rate,'MLP__random_state':[42], 'MLP__max_iter':[30000]}
 # Fit and Evaluate
-GS_MLP = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',scoring='roc_auc')
+GS_MLP = dcv.GridSearchCV(pipeline,parameters,scheduler='threading',cv=pds)
 GS_MLP.fit(X_train, y_train)
 GS_MLP.score(X_test, y_test)
 y_pred = GS_MLP.predict(X_test)
@@ -262,15 +299,17 @@ MLP_AUC = auc(fpr_MLP, tpr_MLP)
 
 ########################## SVM
 steps = [('scaler', StandardScaler()),
+("PCA",PCA()),
 ('SVM', SVC())]
 pipeline = Pipeline(steps)
 # Hyperperameters
+n_components = [20,40,80,120,190]
 C = [0.1, 1, 10, 100, 1000]
 gamma = [1, 0.1, 0.01, 0.001, 0.0001]
 kernel = ['linear', 'poly']
-parameters = {'SVM__C':C, 'SVM__gamma':gamma, 'SVM__kernel':kernel, 'SVM__random_state':[42],'SVM__probability':[True]}
+parameters = {"PCA__n_components":n_components,'SVM__C':C, 'SVM__gamma':gamma, 'SVM__kernel':kernel, 'SVM__random_state':[42],'SVM__probability':[True]}
 # Fit and Evaluate
-GS_SVM = dcv.RandomizedSearchCV(pipeline,parameters,scheduler='threading',random_state=42, n_iter=20,scoring='roc_auc')
+GS_SVM = dcv.RandomizedSearchCV(pipeline,parameters,scheduler='threading',random_state=42, n_iter=20,cv=pds)
 GS_SVM.fit(X_train, y_train)
 GS_SVM.score(X_test, y_test)
 y_pred = GS_SVM.predict(X_test)
@@ -281,6 +320,8 @@ GS_SVM.predict_proba(X_test)
 fpr_SVM, tpr_SVM, thresholds_SVM = roc_curve(y_test, y_pred_prob, pos_label="Buy")
 SVM_AUC = auc(fpr_SVM, tpr_SVM)
 
+
+SVM_AUC=None
 # Track AUC scores
 AUC_scores =  [['Logistic Regression',LogisticRegression_AUC],
                 ['Random Forest',RandomForest_AUC],
